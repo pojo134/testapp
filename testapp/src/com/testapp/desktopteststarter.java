b9ab0343268bf0f8bb3683189bfd73fb216451ab
Brad Mullis <pojo134@gmail.com>
2011-04-29 06:13:15 -0400

    v 0.04

--- testapp/src/com/testapp/desktopteststarter.java
+++ testapp/src/com/testapp/desktopteststarter.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.jogl.JoglApplication;
 public class desktopteststarter {
 	public static void main(String[] args){
 		new JoglApplication(new testappexample(),
-				"test title",
+				"Super Awesome Winning Game!",
 				480,320, false);
 		
 	}
--- testapp/src/com/testapp/testappexample.java
+++ testapp/src/com/testapp/testappexample.java
@@ -22,11 +22,13 @@ import javax.media.opengl.GL;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
@@ -40,36 +42,55 @@ import com.badlogic.gdx.graphics.GLCommon;
 public class testappexample implements ApplicationListener {
 	Input input;
 	Vector3 touchpoint;
-	OrthographicCamera guiCam;
+	//OrthographicCamera guiCam;
 	SpriteBatch spriteBatch;
     BitmapFont font;
+    BitmapFont winText;
     Vector2 textPosition = new Vector2(100, 100);
     Vector2 textDirection = new Vector2(1, 1);
     Texture texture;
     Rectangle winBounds;
     Texture background;
+    TextureRegion backgroundRegion;
+    Sound win;
+    boolean justWon;
+	static final float FRUSTUM_WIDTH = 10;
+	static final float FRUSTUM_HEIGHT = 15;
+	float screenWidth;
+	float screenHeight;
 
 	@Override
 	public void create() {
 		// TODO Auto-generated method stub
-		guiCam = new OrthographicCamera(320, 480);
+		//guiCam = new OrthographicCamera(FRUSTUM_WIDTH,FRUSTUM_HEIGHT);
+		//guiCam.position.set(FRUSTUM_WIDTH/2, FRUSTUM_HEIGHT/2, 0);
 		touchpoint = new Vector3();
 		font = new BitmapFont();
         font.setColor(Color.RED);
+        winText = new BitmapFont();
+        winText.setColor(Color.BLACK);
+        //winText.setScale(10);
         spriteBatch = new SpriteBatch();
+        //spriteBatch.setProjectionMatrix(guiCam.combined);
         texture = new Texture(Gdx.files.internal("data/face.gif"));
-        //background = new Texture(Gdx.files.internal("data/background.png"));
+        background = new Texture(Gdx.files.internal("data/background.png"));
+        backgroundRegion = new TextureRegion(background, 0, 0, 320, 480);
 		Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 1);
 		winBounds = new Rectangle(Gdx.graphics.getWidth()/2 -25,
 				Gdx.graphics.getHeight()/2 -25,
 				50,
 				50 );
+		
+		win = Gdx.audio.newSound(Gdx.files.internal("data/win.mp3"));
+		justWon = false;
+		
+		screenHeight = Gdx.graphics.getHeight();
+		screenWidth = Gdx.graphics.getWidth();
 	}
 
 	@Override
 	public void dispose() {
 
-		
 	}
 
 	@Override
@@ -88,27 +109,40 @@ public class testappexample implements ApplicationListener {
 			touchpoint.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			}
 
-		guiCam.update();
+		//guiCam.update();
 		int centerX = Gdx.graphics.getWidth() / 2;
         int centerY = Gdx.graphics.getHeight() / 2;
         spriteBatch.begin();
         spriteBatch.setColor(Color.WHITE);
-        //spriteBatch.draw(background, 0, 0);
+
+        //spriteBatch.disableBlending();
+        spriteBatch.draw(backgroundRegion, 0f, 0f, screenWidth, screenHeight);
         spriteBatch.draw(texture, 
-                touchpoint.x, 
-                -(touchpoint.y) + Gdx.graphics.getHeight() , 
+                touchpoint.x - texture.getWidth()/2, 
+                -touchpoint.y + Gdx.graphics.getHeight() - texture.getHeight()/2, 
                 0, 0, texture.getWidth(), texture.getHeight());
         font.draw(spriteBatch, touchpoint.x + " " + -touchpoint.y + " " + Gdx.graphics.getFramesPerSecond(), 20, 20);
-        spriteBatch.end();
+        winText.draw(spriteBatch, "Touch here to win", centerX - 50, centerY+50);
         if (OverlapTester.pointInRectangle(winBounds, touchpoint.x, touchpoint.y)){
-        	Gdx.gl.glClearColor(0, 1, 0, 1);
-        }else Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 1);
+        	if(justWon == false){
+            	win.play();
+            	justWon = true;
+        	}
+
+        	
+        }else {
+            justWon = false;
+        }
+        spriteBatch.end();
+        
 
 	}
 
 	@Override
 	public void resize(int arg0, int arg1) {
 		// TODO Auto-generated method stub
+		screenHeight = Gdx.graphics.getHeight();
+		screenWidth = Gdx.graphics.getWidth();
 		
 	}
 
